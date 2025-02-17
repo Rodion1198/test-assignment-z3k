cc3d302325d2861fd3cddff4d24b5d52480a2abb
Rodion <rodka1198@gmail.com>
2025-02-17 17:43:27 +0200

    remove prints

--- apps/redirect/views.py
+++ apps/redirect/views.py
@@ -17,10 +17,6 @@ class PrivateRedirectView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, redirect_identifier):
-        print("Request user:", request.user)
-        print("Redirect Identifier:", redirect_identifier)
-
         rule = get_object_or_404(RedirectRule, redirect_identifier=redirect_identifier, is_private=True,
                                  owner=request.user)
-        print(rule.redirect_url)
         return redirect(rule.redirect_url)
